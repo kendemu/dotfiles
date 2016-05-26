0fe0969a2b2041a47dd56bee22b7e33243ed15b2
kendemu <kendemu@me.com>
2016-05-26 11:44:23 +0900

    add flycheck

---
+++ .emacs.d/elpa/sed-mode-1.0/sed-mode-autoloads.el
@@ -0,0 +1,31 @@
+;;; sed-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (sed-mode) "sed-mode" "sed-mode.el" (22328 31941
+;;;;;;  633338 36000))
+;;; Generated autoloads from sed-mode.el
+ (add-to-list 'auto-mode-alist '("\\.sed\\'" . sed-mode))
+ (add-to-list 'interpreter-mode-alist '("sed" . sed-mode))
+
+(autoload 'sed-mode "sed-mode" "\
+Sed editing mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("sed-mode-pkg.el") (22328 31941 715544
+;;;;;;  214000))
+
+;;;***
+
+(provide 'sed-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; sed-mode-autoloads.el ends here
---
+++ .emacs.d/elpa/sed-mode-1.0/sed-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "sed-mode" "1.0" "Major mode to edit sed scripts" (quote nil))
